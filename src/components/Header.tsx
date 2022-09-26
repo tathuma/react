f7347adb3983850169b793d927246cdde5c5b703
tathuma <stathuma@gmail.com>
2022-09-26 23:17:40 +0900

    second

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles/Header/Header.css'
+
+function Header() {
+    return (
+        
+            <div>
+                
+            </div>
+        
+    )
+}
+
+export default Header;
